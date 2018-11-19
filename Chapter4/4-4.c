2040b35eaeb0767b933d7dc5330a02082a14f53d
ParkEunbin <44806572+ParkEunbin@users.noreply.github.com>
2018-11-19 16:20:02 +0900

    알파벳

--- Chapter4/4-4.c
+++ Chapter4/4-4.c
@@ -1,4 +1,5 @@
-/*#define _CRT_SECURE_NO_WARNINGS
+//알파벳 출력
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #define size 26
 int main() {
@@ -19,4 +20,5 @@ int main() {
 
 	return 0;
 }
-alpabet = { a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z }*/
+
+//alpabet = { a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z }
