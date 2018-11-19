fc68ef0e429b57ac2b8eceee80e484c1a0cb6c10
ParkEunbin <44806572+ParkEunbin@users.noreply.github.com>
2018-11-19 16:02:33 +0900

    산술연산

---
+++ Chapter2/2-7.c
@@ -0,0 +1,15 @@
+//산술연산자 응용
+#include <stdio.h>
+int main(){
+	int a;
+	printf("a=");
+	scanf("%d",&a);
+	
+		if (a%2==0)
+			printf("a´Â Â¦¼ö");
+		else 
+			printf("a´Â È¦¼ö");
+	
+
+	return 0;
+}
