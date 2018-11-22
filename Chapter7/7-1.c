84cda099263d538e72910921fed1d8ad428945ef
ParkEunbin <44806572+ParkEunbin@users.noreply.github.com>
2018-11-22 14:48:58 +0900

    구조체

---
+++ Chapter7/7-1.c
@@ -0,0 +1,21 @@
+//학점기록기
+#include <stdio.h>
+#include <string.h>
+struct score{
+	int num;
+	char name[10];
+	double grade;
+};
+
+int main(){
+	struct score a={111003,"김철수",4.1};
+	struct score b;
+	b.num=103112;
+	strcpy(b.name,"홍길동");
+	b.grade=3.5;
+
+	printf("학번:%d,이름:%s,학점:%.2f\n",a.num,a.name,a.grade);
+	printf("학번:%d,이름:%s,학점:%.2f\n",b.num,b.name,b.grade);
+
+	return 0;
+}
